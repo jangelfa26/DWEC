a9452ffcc8fa9578b490b0e2375cea032f0c9bdc
José Ángel <jangelfa26@educastur.es>
2025-10-22 10:18:53 +0200

    ejerc 07 acabado

--- Ejerc_07/scripts/main.js
+++ Ejerc_07/scripts/main.js
@@ -6,17 +6,30 @@ document.addEventListener('DOMContentLoaded', () => {
     // --- Solución Ejercicio 1 y 4 ---
     let contenedorExterior = document.getElementById("outer-box");
     contenedorExterior.addEventListener("click", (event) =>{
+        let contenedorSeleccionado = event.target;
+        let elementoGestion = event.currentTarget;
+        if (contenedorSeleccionado.id != "middle-box") {
+        contenedorSeleccionado.style.backgroundColor = "coral";
+        console.log(contenedorSeleccionado.id);
+        console.log(elementoGestion.id);    
+        }
+        
+        
+    })
+    let middleBox = document.getElementById("middle-box");
+    middleBox.addEventListener("click", (event) =>{
         let contendorSeleccionado = event.target;
         let elementoGestion = event.currentTarget;
         if (contendorSeleccionado.id == "middle-box") {
-        contendorSeleccionado.style.backgroundColor = "red";
-        event.stopPropagation();    
-        } else {
-        contendorSeleccionado.style.backgroundColor = "coral";
+            event.stopPropagation();
+            middleBox.style.backgroundColor = "coral";
+
+            console.log(contendorSeleccionado.id);
+            console.log(elementoGestion.id);
         }
-        console.log(contendorSeleccionado.id);
-        console.log(elementoGestion.id);
-        // revisar lo que piden en el ejercicio 4, no muy claro si esta correcto
+        
+        
+
     })
 
     // --- Solución Ejercicio 2 ---
@@ -42,6 +55,44 @@ document.addEventListener('DOMContentLoaded', () => {
     })
     // --- Solución Ejercicio 5 ---
     let botonNotificacion = document.getElementById("notification-btn");
+    let areaNotificacion = document.getElementById("notification-area");
 
+
+    botonNotificacion.addEventListener("click", ()=>{
+    const fecha = new Date().toISOString();
+    const eventoNotification = new CustomEvent( 'notification', {
+        detail: {
+            mensaje: 'mensaje ejemplo - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse convallis, felis eget sagittis blandit, lorem.',
+            fecha: fecha
+            }
+        })
+
+        document.dispatchEvent(eventoNotification);
+    });
+
+    document.addEventListener("notification", (event) => {
+        let añadirNotificacion = document.createElement("div");
+        añadirNotificacion.setAttribute("id", "notificacion");
+        
+        console.log(añadirNotificacion);
+
+        let tituloNotificacion = document.createElement("h3");
+        tituloNotificacion.textContent = "Notificacion";
+        
+        añadirNotificacion.appendChild(tituloNotificacion);
+
+        let mensaje = document.createElement("p");
+        mensaje.textContent = event.detail.mensaje;
+
+        añadirNotificacion.appendChild(mensaje);
+
+        let fechaBloque = document.createElement("p");
+        fechaBloque.textContent = event.detail.fecha;
+
+        añadirNotificacion.appendChild(fechaBloque);
+        areaNotificacion.appendChild(añadirNotificacion);
+
+    })
+    
 
 });
