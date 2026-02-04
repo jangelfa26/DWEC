349c19526c68e6e9dfa7d6808f2b6cc5f33c1bb4
José Ángel Frade Álvarez <jangelfa26@educastur.es>
2026-02-04 09:22:41 +0100

    Ejerc_17 acabado

--- Ejerc_17/discoteca/artista/routes.js
+++ Ejerc_17/discoteca/artista/routes.js
@@ -2,25 +2,12 @@ const express = require('express');
 const router = express.Router();
 const controller = require('./controller');
 
-// LISTA
 router.get('/artistas', controller.list);
-
-// FORM NUEVO (SIN :id) → TIENE QUE IR ANTES QUE /artista/:id
 router.get('/artista/form', controller.form);
-
-// FORM EDITAR (CON :id)
 router.get('/artista/edit/:id', controller.form);
-
-// DETALLE (DESPUÉS DE /form y /edit)
 router.get('/artista/:id', controller.detail);
-
-// GUARDAR NUEVO
 router.post('/artista/form', controller.save);
-
-// GUARDAR EDICIÓN
 router.post('/artista/edit/:id', controller.save);
-
-// BORRAR
 router.get('/artista/delete/:id', controller.delete);
 
 module.exports = router;
