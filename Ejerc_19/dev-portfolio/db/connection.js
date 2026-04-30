a7430ca4c873b4484301e2c333497a4120a676f2
jangelfa <jangelfa@educastur.es>
2026-04-30 03:31:42 +0200

    ejerc 19 acabado

--- Ejerc_19/dev-portfolio/db/connection.js
+++ Ejerc_19/dev-portfolio/db/connection.js
@@ -1,4 +1,5 @@
 import mysql from "mysql2/promise";
+import fs from "fs";
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
@@ -9,8 +10,13 @@ const pool = mysql.createPool({
   connectionLimit: 10,
   queueLimit: 0,
   ssl: {
-    rejectUnauthorized: false
+    rejectUnauthorized: false,
+    ca: fs.readFileSync('./global-bundle.pem')
   }
 });
+console.log(process.env.DB_HOST);
+console.log(process.env.DB_USER);
+console.log(process.env.DB_PASSWORD);
+console.log(process.env.DB_NAME);
 
 export default pool;
