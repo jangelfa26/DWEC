76b02c4125c2876e6bd08412e6286c8792826026
jangelfa <jangelfa@educastur.es>
2026-05-12 07:18:43 +0200

    Ejerc 21

--- Ejerc_21/gestion-usuarios/api/index.js
+++ Ejerc_21/gestion-usuarios/api/index.js
@@ -8,13 +8,32 @@ import s3 from '../src/config/s3.js';
 
 import { GetObjectCommand } from '@aws-sdk/client-s3';
 
+import path from 'path';
+
+import { fileURLToPath } from 'url';
+
 const app = express();
 
 app.use(cors());
 
 app.use(express.json());
 
-app.use(express.static('public'));
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
+
+app.use(
+    express.static(
+        path.join(__dirname, '../public')
+    )
+);
+
+app.get('/', (req, res) => {
+
+    res.sendFile(
+        path.join(__dirname, '../public/index.html')
+    );
+});
 
 app.use('/api/alumnos', alumnosRoutes);
 
